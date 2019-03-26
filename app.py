318996850b20d284a3ff30b4548ddae1d46a9066
SeptiD <septid96@yahoo.com>
2019-03-26 21:38:58 +0200

    I004: gr start page

--- app.py
+++ app.py
@@ -3,12 +3,24 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
 from PyQt5.uic import loadUi
 from designer_wrapped import UI_Wrapped
-
-
+import psutil
+from PyQt5 import QtCore
 app = QApplication(sys.argv)
 ex = UI_Wrapped()
 w = QMainWindow()
 ex.setupUi(w)
 
 w.show()
+
+
+# def handler_psutil():
+#     value = psutil.cpu_percent(interval=None, percpu=True)
+#     ex.textEdit.append(str(value))
+#
+#
+# timer = QtCore.QTimer()
+# timer.timeout.connect(handler_psutil)
+# timer.start(1000)
+
+
 sys.exit(app.exec_())
--- designer_wrapped.py
+++ designer_wrapped.py
@@ -2,15 +2,34 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from designer import Ui_MainWindow
 import pyqtgraph as pg
 import multiprocessing
+import psutil
 
 
 class UI_Wrapped(Ui_MainWindow):
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
-        # plot1 = pg.PlotWidget()
-        # self.verticalLayout_cpu_plots.addWidget(plot1)
+        self.frame_progress_bars.hide()
+
         for cpu in range(multiprocessing.cpu_count()):
-            self.verticalLayout_cpu_plots.addWidget(pg.PlotWidget())
+            x = pg.PlotWidget()
+            x.setYRange(0, 100)
+            self.verticalLayout_cpu_plots.addWidget(x)
+            self.verticalLayout_cpu_progress_bars.addWidget(QtWidgets.QProgressBar())
+
+        # self.textEdit.append('start')
+        self.button_cpu_views.clicked.connect(self.cpu_views_button_pushed)
+
+    def cpu_views_button_pushed(self):
+        if self.frame_cpu_plots.isHidden():
+            self.frame_progress_bars.hide()
+            self.frame_cpu_plots.show()
+            self.button_cpu_views.setText('BARS')
+        else:
+            self.frame_progress_bars.show()
+            self.frame_cpu_plots.hide()
+            self.button_cpu_views.setText('PLOTS')
+
+
 
     def retranslateUi(self, MainWindow):
         super().retranslateUi(MainWindow)
