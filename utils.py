cf9d5196235e62b383f6527db59f4d79f81f64a2
SeptiD <septid96@yahoo.com>
2019-03-29 17:47:39 +0200

    I006: implemented colors

--- utils.py
+++ utils.py
@@ -1,23 +1,26 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class CustomProgressBar(QtGui.QProgressBar):
+class CustomProgressBar(QtWidgets.QProgressBar):
     th1 = 40
     th2 = 80
 
+    def __init__(self, parent = None):
+        # super.__init__(parent)
+        QtWidgets.QProgressBar.__init__(self, parent)
+        self.customPallete = QtGui.QPalette(self.palette())
+
     def setValue(self, value):
         QtGui.QProgressBar.setValue(self, value)
-        if value < 40:
-            palette = QtGui.QPalette(self.palette())
-            palette.setColor(QtGui.QPalette.Highlight,
+        # palette = QtGui.QPalette(self.palette())
+
+        if value < CustomProgressBar.th1:
+            self.customPallete.setColor(QtGui.QPalette.Highlight,
                              QtGui.QColor(QtCore.Qt.blue))
-        elif value >= 40 and value < 80:
-            palette = QtGui.QPalette(self.palette())
-            palette.setColor(QtGui.QPalette.Highlight,
+        elif CustomProgressBar.th1 <= value < CustomProgressBar.th2:
+            self.customPallete.setColor(QtGui.QPalette.Highlight,
                              QtGui.QColor(QtCore.Qt.darkYellow))
-            self.setPalette(palette)
         else:
-            palette = QtGui.QPalette(self.palette())
-            palette.setColor(QtGui.QPalette.Highlight,
+            self.customPallete.setColor(QtGui.QPalette.Highlight,
                              QtGui.QColor(QtCore.Qt.red))
-            self.setPalette(palette)
+        self.setPalette(self.customPallete)
