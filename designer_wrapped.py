5ad91a3858a4d10fd690313702b9e84c332df010
SeptiD <septid96@yahoo.com>
2019-04-14 18:14:13 +0300

    Lots of shit happened, refactored, implemented plot only when on(I028), put on one page cpu plots and percentage bars

--- designer_wrapped.py
+++ designer_wrapped.py
@@ -12,7 +12,6 @@ from subprocess import PIPE, Popen
 from threading import Thread
 from queue import Queue, Empty
 
-
 ON_POSIX = 'posix' in sys.builtin_module_names
 
 
@@ -39,6 +38,7 @@ class CPU_Info:
         self.cpu_plots_data_lists = []
         self.cpu_plots_curves_list = []
         self.cpu_p_bars_list = []
+
         for cpu in range(CPU_Info.nr_of_cpues):
             # setup real-time plots
             temp_plot = pg.PlotWidget(axisItems={'bottom': TimeAxisItem(orientation='bottom')}, title='CPU ' + str(cpu))
@@ -51,6 +51,7 @@ class CPU_Info:
 
             # setup progress bars
             temp_p_bar = utils.CustomProgressBar()
+            temp_p_bar.setOrientation(QtCore.Qt.Vertical)
             self.cpu_p_bars_list.append(temp_p_bar)
 
     def integrate(self, wrapper):
@@ -63,17 +64,18 @@ class CPU_Info:
             else:
                 column -= 1
                 row += 1
-        for temp_p_bar in self.cpu_p_bars_list:
-            wrapper.verticalLayout_cpu_progress_bars.addWidget(temp_p_bar)
 
-    def change_info(self):
+        for temp_p_bar in self.cpu_p_bars_list:
+            wrapper.horizontalLayout_cpu_progress_bars.addWidget(temp_p_bar)
+
+    def change_info(self, active_widget=False):
         cpu_perc_list = psutil.cpu_percent(interval=None, percpu=True)
         # self.cpu_plots_X_values.insert(0, time())
         for cpu_index in range(len(cpu_perc_list)):
             self.cpu_plots_data_lists[cpu_index].insert(0, cpu_perc_list[cpu_index])
-            self.cpu_plots_curves_list[cpu_index].setData(y=self.cpu_plots_data_lists[cpu_index])
-
-            self.cpu_p_bars_list[cpu_index].setValue(cpu_perc_list[cpu_index])
+            if active_widget:
+                self.cpu_plots_curves_list[cpu_index].setData(y=self.cpu_plots_data_lists[cpu_index])
+                self.cpu_p_bars_list[cpu_index].setValue(cpu_perc_list[cpu_index])
 
 
 class CPU_Extra_Info:
@@ -108,26 +110,26 @@ class CPU_Extra_Info:
         for elem in self.info_list:
             wrapper.verticalLayout_cpu_extra_info.addWidget(elem)
 
-    def change_info(self):
+    def change_info(self, active_widget=False):
         info_touple = psutil.cpu_stats()
         temperature_tuples = psutil.sensors_temperatures()['coretemp']
         battery_tuple = psutil.sensors_battery()
 
-        self.label_ctx_switches.setText("Context Switches:" + str(info_touple.ctx_switches))
-        self.label_interrupts.setText("Interrupts:" + str(info_touple.interrupts))
-        self.label_soft_interrupts.setText("Software Interrupts:" + str(info_touple.soft_interrupts))
-        self.up_time = self.up_time + timedelta(0,1)
-        self.label_up_time.setText("Up Time:" + str(self.up_time))
+        if active_widget:
+            self.label_ctx_switches.setText("Context Switches:" + str(info_touple.ctx_switches))
+            self.label_interrupts.setText("Interrupts:" + str(info_touple.interrupts))
+            self.label_soft_interrupts.setText("Software Interrupts:" + str(info_touple.soft_interrupts))
+            self.up_time = self.up_time + timedelta(0, 1)
+            self.label_up_time.setText("Up Time:" + str(self.up_time))
 
+            self.p_bar_battery.setValue(battery_tuple.percent)
+            self.p_bar_battery.setFormat("Battery: " + str(round(battery_tuple.percent, 1)) + '%')
 
-        self.p_bar_battery.setValue(battery_tuple.percent)
-        self.p_bar_battery.setFormat("Battery: " + str(round(battery_tuple.percent, 1)) + '%')
-
-        temperature_tuples.pop(0)
-        for index in range(len(temperature_tuples)):
-            self.just_temperature_list[index].setValue(temperature_tuples[index].current)
-            self.just_temperature_list[index].setFormat(
-                'Core ' + str(index) + ' temperature: ' + str(temperature_tuples[index].current) + '°C')
+            temperature_tuples.pop(0)
+            for index in range(len(temperature_tuples)):
+                self.just_temperature_list[index].setValue(temperature_tuples[index].current)
+                self.just_temperature_list[index].setFormat(
+                    'Core ' + str(index) + ' temperature: ' + str(temperature_tuples[index].current) + '°C')
 
 
 class Memory_Info:
@@ -143,14 +145,14 @@ class Memory_Info:
         self.treeview_disk_part_info = QtWidgets.QTreeWidget()
         self.treeview_disk_part_info.setHeaderLabels(self.header_labels)
 
-        self.change_info()
+        self.change_info(active_widget=True)
 
     def integrate(self, wrapper):
         wrapper.verticalLayout_memory_info.addWidget(self.treeview_disk_part_info)
         wrapper.verticalLayout_memory_info.addWidget(self.label_disk_usage)
         wrapper.verticalLayout_memory_info.addWidget(self.p_bar_diskspace)
 
-    def change_info(self):
+    def change_info(self, active_widget=False):
         self.set_disk_usage()
 
         now_disks = set()
@@ -204,13 +206,13 @@ class Network_Info:
         self.treeview_net_io_info.setHeaderLabels(self.net_io_headers)
         self.treeview_net_io_info.setSortingEnabled(True)
 
-        self.change_info()
+        self.change_info(active_widget=True)
 
     def integrate(self, wrapper):
         wrapper.verticalLayout_network_info.addWidget(self.treeview_net_con)
         wrapper.verticalLayout_network_info.addWidget(self.treeview_net_io_info)
 
-    def change_info(self):
+    def change_info(self, active_widget=False):
         self.set_net_connections()
         self.set_net_io_counters()
 
@@ -312,12 +314,12 @@ class Processes_Info:
         self.treeview_processes_info.addAction(self.kill_process_action)
 
         self.processes_rows = {}
-        self.change_info()
+        self.change_info(active_widget=True)
 
     def integrate(self, wrapper):
         wrapper.verticalLayout_processes_info.addWidget(self.treeview_processes_info)
 
-    def change_info(self):
+    def change_info(self, active_widget=False):
         now_pids = set()
         for proc in psutil.process_iter(attrs=self.header_labels):
             proc_dict = proc.info
@@ -326,23 +328,25 @@ class Processes_Info:
             proc_dict['memory_info'] = round(proc_dict['memory_info'].vms / (1024 * 1024), 2)
             temp_list = [str(proc_dict[elem]) for elem in self.header_labels]
 
-            if proc_dict['pid'] not in self.processes_rows.keys():
-                temp_widget_item = QtWidgets.QTreeWidgetItem(self.treeview_processes_info, temp_list)
-                self.processes_rows[proc_dict['pid']] = temp_widget_item
-            else:
-                temp_widget_item = self.processes_rows[proc_dict['pid']]
-                for idx in range(len(temp_list)):
-                    temp_widget_item.setText(idx, temp_list[idx])
+            if active_widget:
+                if proc_dict['pid'] not in self.processes_rows.keys():
+                    temp_widget_item = QtWidgets.QTreeWidgetItem(self.treeview_processes_info, temp_list)
+                    self.processes_rows[proc_dict['pid']] = temp_widget_item
+                else:
+                    temp_widget_item = self.processes_rows[proc_dict['pid']]
+                    for idx in range(len(temp_list)):
+                        temp_widget_item.setText(idx, temp_list[idx])
 
             now_pids.add(proc_dict['pid'])
 
-        for key, current in self.processes_rows.items():
-            if key not in now_pids:
-                if current.parent() is not None:
-                    current.parent().removeChild(current)
-                else:
-                    self.treeview_processes_info.takeTopLevelItem(
-                        self.treeview_processes_info.indexOfTopLevelItem(current))
+        if active_widget:
+            for key, current in self.processes_rows.items():
+                if key not in now_pids:
+                    if current.parent() is not None:
+                        current.parent().removeChild(current)
+                    else:
+                        self.treeview_processes_info.takeTopLevelItem(
+                            self.treeview_processes_info.indexOfTopLevelItem(current))
 
     def kill_proc(self):
         my_item = self.treeview_processes_info.currentItem()
@@ -399,7 +403,7 @@ class HPC_Info:
             for row in range(len(self.hpc_plots[0])):
                 wrapper.gridLayout_hpc_info.addWidget(self.hpc_plots[column][row], row, column)
 
-    def change_info(self):
+    def change_info(self, active_widget=False):
         # if not self.check:
         #     self.check = True
         # self.textEdit_hpc.append('-----------\n')
@@ -413,7 +417,8 @@ class HPC_Info:
             except Empty:
                 break
 
-        self.update_curves()
+        if active_widget:
+            self.update_curves()
 
     def update_data(self, input_line):
         #  1.000418172 CPU1                40.462      r205
@@ -440,36 +445,27 @@ class UI_Wrapped(Ui_MainWindow):
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
 
-        self.setup_combobox_system_info()
-
-        self.elements = []
-        self.elements.append(CPU_Info())
-        self.elements.append(CPU_Extra_Info())
-        self.elements.append(Memory_Info())
-        self.elements.append(Network_Info())
-        self.elements.append(Processes_Info())
-        self.elements.append(HPC_Info())
+        self.elements = {}
+        self.elements[0] = CPU_Info()
+        self.elements[1] = CPU_Extra_Info()
+        self.elements[2] = Memory_Info()
+        self.elements[3] = Network_Info()
+        self.elements[4] = Processes_Info()
+        self.elements[5] = HPC_Info()
 
         self.integrate_all()
-
-    def setup_combobox_system_info(self):
-        self.comboBox_system_info.addItems(UI_Wrapped.combobox_system_info_options)
-        self.comboBox_system_info.activated[str].connect(self.combobox_system_info_selected)
-
-    def combobox_system_info_selected(self, combo_text):
-        self.textEdit.append(combo_text)
-        pass
-
-    def cpu_views_button_pushed(self):
-        pass
 
     def retranslateUi(self, MainWindow):
         super().retranslateUi(MainWindow)
 
     def integrate_all(self):
-        for elem in self.elements:
+        for key, elem in self.elements.items():
             elem.integrate(self)
 
     def update_all(self):
-        for elem in self.elements:
-            elem.change_info()
+        current_index = self.tabWidget.currentIndex()
+        for key, value in self.elements.items():
+            if key != current_index:
+                value.change_info()
+            else:
+                value.change_info(active_widget=True)
